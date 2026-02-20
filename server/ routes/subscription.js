5f39d812ad66f4be51eca0ac7d95574026313f2d
mohammad123443214554 <khankadir2703@gmail.com>
2026-02-20 23:32:52 +0530

    Update subscription.js

--- server/ routes/subscription.js
+++ server/ routes/subscription.js
@@ -1,1 +1,23 @@
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+router.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: { name: "Aperonix Pro Plan" },
+          unit_amount: 29900,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+  });
+
+  res.json({ id: session.id });
+});
