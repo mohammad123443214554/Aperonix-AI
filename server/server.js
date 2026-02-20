a4d12baa3610a85ad84f604da4f745e1a6160db3
mohammad123443214554 <khankadir2703@gmail.com>
2026-02-20 23:31:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import chatRoute from "./routes/chat.js";
+import imageRoute from "./routes/image.js";
 
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/chat", chatRoute);
+app.use("/api/image", imageRoute);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
